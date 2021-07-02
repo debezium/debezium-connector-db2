8d70a33ed72106846482b28f52768f8e69fb628d
Chris Cranford <chris@hibernate.org>
2021-07-02 07:25:01 -0400

    DBZ-3668 Add ServiceLoader hook CloudEvents converter

---
+++ src/main/java/io/debezium/connector/db2/converters/Db2CloudEventsMaker.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.db2.converters;
+
+import io.debezium.connector.AbstractSourceInfo;
+import io.debezium.converters.CloudEventsMaker;
+import io.debezium.converters.RecordParser;
+import io.debezium.converters.SerializerType;
+
+/**
+ * CloudEvents maker for records produced by the Db2 connector.
+ *
+ * @author Chris Cranford
+ */
+public class Db2CloudEventsMaker extends CloudEventsMaker {
+
+    public Db2CloudEventsMaker(RecordParser parser, SerializerType contentType, String dataSchemaUriBase) {
+        super(parser, contentType, dataSchemaUriBase);
+    }
+
+    @Override
+    public String ceId() {
+        return "name:" + recordParser.getMetadata(AbstractSourceInfo.SERVER_NAME_KEY)
+                + ";change_lsn:" + recordParser.getMetadata(Db2RecordParser.CHANGE_LSN_KEY)
+                + ";commit_lsn:" + recordParser.getMetadata(Db2RecordParser.COMMIT_LSN_KEY);
+    }
+}
---
+++ src/main/java/io/debezium/connector/db2/converters/Db2RecordParser.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.db2.converters;
+
+import java.util.Set;
+
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.errors.DataException;
+
+import io.debezium.converters.RecordParser;
+import io.debezium.data.Envelope;
+import io.debezium.util.Collect;
+
+/**
+ * Parser for records produced by the Db2 connector.
+ *
+ * @author Chris Cranford
+ */
+public class Db2RecordParser extends RecordParser {
+
+    static final String CHANGE_LSN_KEY = "change_lsn";
+    static final String COMMIT_LSN_KEY = "commit_lsn";
+
+    static final Set<String> DB2_SOURCE_FIELD = Collect.unmodifiableSet(
+            CHANGE_LSN_KEY,
+            COMMIT_LSN_KEY);
+
+    public Db2RecordParser(Schema schema, Struct record) {
+        super(schema, record, Envelope.FieldName.BEFORE, Envelope.FieldName.AFTER);
+    }
+
+    @Override
+    public Object getMetadata(String name) {
+        if (SOURCE_FIELDS.contains(name)) {
+            return source().get(name);
+        }
+        if (DB2_SOURCE_FIELD.contains(name)) {
+            return source().get(name);
+        }
+
+        throw new DataException("No such field \"" + name + "\" in the \"source\" field of events from Db2 connector");
+    }
+}
