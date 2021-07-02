d62af61b5a8f07a878463368c7400822dfd8f8e9
Chris Cranford <chris@hibernate.org>
2021-07-02 07:25:01 -0400

    DBZ-3668 Add ServiceLoader hook CloudEvents converter

---
+++ src/main/java/io/debezium/connector/db2/converters/Db2CloudEventsProvider.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.db2.converters;
+
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.Struct;
+
+import io.debezium.connector.db2.Module;
+import io.debezium.converters.CloudEventsMaker;
+import io.debezium.converters.CloudEventsProvider;
+import io.debezium.converters.RecordParser;
+import io.debezium.converters.SerializerType;
+
+/**
+ * An implementation of {@link CloudEventsProvider} for Db2.
+ *
+ * @author Chris Cranford
+ */
+public class Db2CloudEventsProvider implements CloudEventsProvider {
+    @Override
+    public String getName() {
+        return Module.name();
+    }
+
+    @Override
+    public RecordParser createParser(Schema schema, Struct record) {
+        return new Db2RecordParser(schema, record);
+    }
+
+    @Override
+    public CloudEventsMaker createMaker(RecordParser parser, SerializerType contentType, String dataSchemaUriBase) {
+        return new Db2CloudEventsMaker(parser, contentType, dataSchemaUriBase);
+    }
+}
