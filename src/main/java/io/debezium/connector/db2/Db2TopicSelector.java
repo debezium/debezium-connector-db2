4c69ece7ffcb8ad657ddcd501290e5bd1e416843
harveyyue <yw_yuewei@126.com>
2022-07-28 11:52:34 +0800

    DBZ-5457 Deprecate legacy topic selector for all connectors

--- src/main/java/io/debezium/connector/db2/Db2TopicSelector.java
+++ src/main/java/io/debezium/connector/db2/Db2TopicSelector.java
@@ -12,8 +12,9 @@ import io.debezium.schema.TopicSelector;
  * The topic naming strategy based on connector configuration and table name
  *
  * @author Jiri Pechanec
- *
+ * @deprecated Use {@link io.debezium.schema.SchemaTopicNamingStrategy} instead.
  */
+@Deprecated
 public class Db2TopicSelector {
 
     public static TopicSelector<TableId> defaultSelector(Db2ConnectorConfig connectorConfig) {
