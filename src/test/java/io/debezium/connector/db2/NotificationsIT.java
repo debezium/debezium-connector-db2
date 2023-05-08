998eedbf1ee723ef27aa19fbd0f10b21ebadbd7c
mfvitale <mvitale@redhat.com>
2023-05-08 11:13:43 +0200

    DBZ-1973 Add support for Notifications

---
+++ src/test/java/io/debezium/connector/db2/NotificationsIT.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.debezium.connector.db2;
+
+import java.sql.SQLException;
+
+import org.junit.After;
+import org.junit.Before;
+
+import io.debezium.config.Configuration;
+import io.debezium.connector.db2.util.TestHelper;
+import io.debezium.pipeline.notification.AbstractNotificationsIT;
+import io.debezium.util.Testing;
+
+public class NotificationsIT extends AbstractNotificationsIT<Db2Connector> {
+
+    private Db2Connection connection;
+
+    @Before
+    public void before() throws SQLException {
+
+        connection = TestHelper.testConnection();
+
+        initializeConnectorTestFramework();
+        Testing.Files.delete(TestHelper.DB_HISTORY_PATH);
+        Testing.Print.enable();
+    }
+
+    @After
+    public void after() throws SQLException {
+        if (connection != null) {
+            TestHelper.disableDbCdc(connection);
+
+            connection.close();
+        }
+    }
+
+    @Override
+    protected Class<Db2Connector> connectorClass() {
+        return Db2Connector.class;
+    }
+
+    @Override
+    protected Configuration.Builder config() {
+        return TestHelper.defaultConfig()
+                .with(Db2ConnectorConfig.SNAPSHOT_MODE, Db2ConnectorConfig.SnapshotMode.INITIAL);
+    }
+
+    @Override
+    protected String snapshotStatusResult() {
+        return "COMPLETED";
+    }
+}
