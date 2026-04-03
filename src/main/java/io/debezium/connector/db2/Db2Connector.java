c1f5ee7cf38a54b1582c84ab6b2ca2d32a13f4e3
Chris Cranford <chris@hibernate.org>
2026-04-03 07:57:06 -0400

    debezium/dbz#1787 Remove unused getMatchingCollections method
    Signed-off-by: Chris Cranford <chris@hibernate.org>

--- src/main/java/io/debezium/connector/db2/Db2Connector.java
+++ src/main/java/io/debezium/connector/db2/Db2Connector.java
@@ -6,7 +6,6 @@
 package io.debezium.connector.db2;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -18,12 +17,10 @@ import org.apache.kafka.connect.connector.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.debezium.DebeziumException;
 import io.debezium.annotation.ThreadSafe;
 import io.debezium.config.Configuration;
 import io.debezium.connector.common.RelationalBaseSourceConnector;
 import io.debezium.relational.RelationalDatabaseConnectorConfig;
-import io.debezium.relational.TableId;
 
 /**
  * The main connector class used to instantiate configuration and execution classes
@@ -98,16 +95,4 @@ public class Db2Connector extends RelationalBaseSourceConnector {
         return config.validate(Db2ConnectorConfig.ALL_FIELDS);
     }
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public List<TableId> getMatchingCollections(Configuration config) {
-        Db2ConnectorConfig connectorConfig = new Db2ConnectorConfig(config);
-        try (Db2Connection connection = new Db2Connection(connectorConfig)) {
-            return new ArrayList<>(
-                    connection.readTableNames(null, null, null, new String[]{ "TABLE" }));
-        }
-        catch (SQLException e) {
-            throw new DebeziumException(e);
-        }
-    }
 }
